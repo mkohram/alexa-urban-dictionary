b6f544c5299d8f72846c73a740335032928d4f2d
Marcus Renno <renno.marcus@gmail.com>
2018-07-10 12:52:39 -0400

    Added comments

--- urban-dictionary.py
+++ urban-dictionary.py
@@ -11,10 +11,14 @@ URBAN_DICTIONARY_URL = 'http://api.urbandictionary.com/v0/define?term='
 
 @ask.launch
 def launch():
+    '''launch is called when the skill is started'''
+
     speech_text = 'Welcome to the Urban Dictionary, ask for a definition'
     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
 
 def find_best(def_list):
+    '''find_best returns the most voted result from the given list'''
+
     max_thumbs = 0
     choice = None
     for option in def_list:
@@ -24,7 +28,8 @@ def find_best(def_list):
     return choice
 
 def get_term(term):
-    ''' return a json object according to the term given '''
+    '''get_term returns a json object according to the given term'''
+
     r = requests.get(URBAN_DICTIONARY_URL+term)
     result = r.json()
     if result['result_type'] == 'no_results':
@@ -33,6 +38,9 @@ def get_term(term):
 
 @ask.intent('AskWord', mapping={'term': 'Term'})
 def ask_word(term):
+    '''ask_word is the intent function responsible for creating the request and 
+    returning the appropriate result'''
+
     def_dictionary = get_term(term)
     if def_dictionary:
         choice = find_best(def_dictionary['list'])
@@ -49,6 +57,9 @@ def ask_word(term):
 
 @ask.intent('Example', mapping={'term': 'Term'})
 def ask_example(term):
+    '''ask_example is the intent function responsbile for returning an example from
+    the given term'''
+
     if term:
         def_dictionary = get_term(term)
         if def_dictionary:
@@ -73,11 +84,15 @@ def ask_example(term):
 
 @ask.intent('AMAZON.StopIntent')
 def stop():
+    '''stop is the intent responsible to stop the skill'''
+
     text = render_template('goodbye')
     return statement(text)
 
 @ask.intent('AMAZON.CancelIntent')
 def cancel():
+    '''cancel is the intent resposnble to cancel any request of the skill'''
+
     text = render_template('goodbye')
     return statement(text)
 
